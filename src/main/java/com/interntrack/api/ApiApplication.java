ff31ff5db6f56959b91c879a8e8a0194833d99d1
fadime <kovalikfadime@gmail.com>
2026-08-09 09:04:30 +0300

    fix: resolve Swagger UI fetch errors by using relative server URL

--- src/main/java/com/interntrack/api/ApiApplication.java
+++ src/main/java/com/interntrack/api/ApiApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -17,7 +18,8 @@ import org.springframework.cache.annotation.EnableCaching;
 				version = "1.0",
 				description = "Internship Application Tracking System API Documentation"
 		),
-		security = @SecurityRequirement(name = "Bearer Token")
+		security = @SecurityRequirement(name = "Bearer Token"),
+		servers = {@Server(url = "/", description = "Default Server URL")}
 )
 @SecurityScheme(
 		name = "Bearer Token",
